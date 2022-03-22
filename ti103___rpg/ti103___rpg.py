c5940d245d47a418fc79dde76ec2d663e6997d51
fallou <fallouc10@gmail.com>
2022-03-22 18:05:20 -0500

    interface noir

--- ti103___rpg/ti103___rpg.py
+++ ti103___rpg/ti103___rpg.py
@@ -1,1 +1,13 @@
 """Main module."""
+import pygame
+pygame.init()
+window = pygame.display.set_mode((800, 600))
+run = True
+
+
+while run:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run = False
+
+pygame.quit()
